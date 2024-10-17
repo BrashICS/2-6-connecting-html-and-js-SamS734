d6d800253eb31412ed29a7c1ab47f83ffdb6a489
Sam <samuel.sum@stu.ocsb.ca>
2024-10-17 10:42:24 -0400

    euasgfuoasfoajhf

--- main.js
+++ main.js
@@ -9,9 +9,56 @@
 'use strict';
 
 /*** Event Listeners ***/
+document.getElementById("d6").addEventListener("click", roll_d6);
 
+document.getElementById("d8").addEventListener("click", roll_d8);
+
+document.getElementById("d6").addEventListener("mouseenter", d6_enter);
+
+document.getElementById("d6").addEventListener("mouseleave", d6_leave);
+
+document.getElementById("enter").addEventListener("click", greeting);
 
 /*** Functions ***/
+function roll_d6() {
+
+    let roll = randInt(1, 6);
+
+    document.getElementById("die_roll").textContent = roll;
+
+}
+
+
+function roll_d8() {
+
+    let roll = randInt(1, 8);
+
+    document.getElementById("die_roll").textContent = roll;
+
+}
+
+
+function d6_enter() {
+
+document.getElementById("die").textContent = "🧏";
+
+}
+
+function d6_leave() {
+
+    document.getElementById("die").textContent = "🎲";
+
+}
+
+
+function greeting() {
+
+    let name = document.getElementById("user_input").value;
+
+    document.getElementById("greeting").textContent = `Hello ${name}!`;
+
+}
+
 
 // Return a random integer from `min` to `max`, inclusive
 function randInt(min, max) {
